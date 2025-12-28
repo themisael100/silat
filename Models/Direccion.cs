a31ffe8cb2ba22f5ed249d5eef7ab393c5bd8631
themisael100 <misalbryan@gmail.com>
2025-12-28 13:56:57 -0600

    feat(Models): Creación del modelo de Dirección

--- Models/Direccion.cs
+++ Models/Direccion.cs
@@ -1,8 +1,33 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace silat.Models;
 
 public class Direccion
 {
+    [Key]
+    public int DireccionId { get; set; }
 
+    [Required]
+    [StringLength(255)]
+    public string Adress { get; set; } = null!;
+
+    [Required]
+    [StringLength(40)]
+    public string Ciudad { get; set; } = null!;
+
+    [Required]
+    [StringLength(20)]
+    public string Estado { get; set; } = null!;
+
+    [Required]
+    [StringLength(10)]
+    public string CodigoPostal { get; set; } = null!;
+
+    [Required]
+    public int UsuarioId { get; set; }
+
+    [ForeignKey("UsuarioId")]
+    public Usuario Usuario { get; set; } = null!;
 }

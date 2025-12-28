3117ed0849ba6e82dcb9497f4872a845c05b477b
themisael100 <misalbryan@gmail.com>
2025-12-28 13:20:13 -0600

    refactor(Models): Configuración de dataannotations

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -6,23 +6,51 @@ namespace silat.Models;
 
 public class Usuario
 {
+    public Usuario()
+    {
+        Pedidos = new List<Pedido>();
+    }
+
     [Key]
     public int UsuarioId { get; set; }
 
-    [Required(ErrorMessage = "EL nombre es obligatorio")]
+    [Required]
     [StringLength(50)]
     public string Nombre { get; set; } = null!;
 
+    [Required]
+    [StringLength(15)]
     public string Telefono { get; set; } = null!;
+
+    [Required]
+    [StringLength(100)]
     public string NombreUsuario { get; set; } = null!;
+
+    [Required]
+    [StringLength(255)]
     public string Contrasenia { get; set; } = null!;
+
+    [Required]
+    [StringLength(255)]
     public string Correo { get; set; } = null!;
+
+    [Required]
+    [StringLength(255)]
     public string Direccion { get; set; } = null!;
+
+    [Required]
+    [StringLength(40)]
     public string Ciudad { get; set; } = null!;
+
+    [Required]
+    [StringLength(20)]
     public string Estado { get; set; } = null!;
+
+    [Required]
+    [StringLength(10)]
     public string CodigoPostal { get; set; } = null!;
 
-
+    [Required]
     public int RolId { get; set; }
 
     [ForeignKey("RolId")]
@@ -30,5 +58,8 @@ public class Usuario
 
 
     public ICollection<Pedido> Pedidos { get; set; }
+
+    [InverseProperty("Usuario")]
     public ICollection<Direccion> Direcciones { get; set; } = null!;
+
 }

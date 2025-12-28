8caadeb1ae23070ba8ec5c617ffc0e36debcddb6
themisael100 <misalbryan@gmail.com>
2025-12-28 14:16:50 -0600

    feat(Models): Creación del modelo de Detalle_Pedido

--- Models/Detalle_Pedido.cs
+++ Models/Detalle_Pedido.cs
@@ -1,8 +1,23 @@
-using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace silat.Models;
 
 public class Detalle_Pedido
 {
-
+    [Key]
+    public int DetallePedidoId { get; set; }
+    [Required]
+    public int PedidoId { get; set; }
+    [ForeignKey("PedidoId")]
+    public Pedido Pedido { get; set; } = null!;
+    [Required]
+    public int ProductoId { get; set; }
+    [ForeignKey("ProductoId")]
+    public Producto Producto { get; set; } = null!;
+    [Required]
+    public int Cantidad { get; set; }
+    [Required]
+    public decimal Precio { get; set; }
+ 
 }

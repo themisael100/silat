92dd62fd0a1d1421e057f58be459d56e854d7505
themisael100 <misalbryan@gmail.com>
2025-12-28 13:47:22 -0600

    feat(Models): Creación del modelo de Categoria

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,8 +1,22 @@
-using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace silat.Models;
 
 public class Categoria
 {
+    public Categoria()
+    {
+        Productos = new List<Producto>();
+    }
+    [Key]
+    public int CategoriaId { get; set; }
+    [Required]
+    [StringLength(100)]
+    public string Nombre { get; set; } = null!;
 
+     [Required]
+    [StringLength(1000)]
+    public string Descripcion { get; set; } = null!;
+
+    public ICollection<Producto> Productos { get; set; }
 }

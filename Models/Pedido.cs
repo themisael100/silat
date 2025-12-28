71daab515a2f678d12be8075e6082173c89c1606
themisael100 <misalbryan@gmail.com>
2025-12-28 14:09:08 -0600

    feat(Models): Creación del modelo de Pedido

--- Models/Pedido.cs
+++ Models/Pedido.cs
@@ -1,8 +1,32 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace silat.Models;
 
 public class Pedido
 {
+
+    [Key]
+    public int PedidoId { get; set; }
+
+    [Required]
+    public int UsuarioId { get; set; }
+
+    [ForeignKey("UsuarioId")]
+    public Usuario Usuario { get; set; } = null!;
+    [Required]
+    public DateTime Fecha { get; set; }
+    [Required]
+    public string Estado { get; set; } = null!;
+
+    [Required]
+    public int DireccionIdSeleccionada { get; set; }
+
+    [ForeignKey("DireccionIdSeleccionada")]
+    public Direccion Direccion { get; set; } = null!;
+    public decimal Total { get; set; }
+    public ICollection<Detalle_Pedido> DetallesPedido { get; set; } = null!;
+
 
 }

912f7a86f7f9c8e87af6662cdee65b810d914938
themisael100 <misalbryan@gmail.com>
2026-01-04 10:40:39 -0600

    feat(Models/ViewModels): Creación del ViewModel del carrito de compras.

---
+++ Models/ViewModels/CarritoItemViewModel.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace silat.Models.ViewModels;
+
+public class CarritoItemViewModel
+{
+    public int ProductoId { get; set; }
+    public Producto Producto { get; set; } = null!;
+    public string Nombre { get; set; } = null!;
+    public decimal Precio { get; set; }
+    public int Cantidad { get; set; }
+    public decimal Subtotal => Precio * Cantidad;
+}

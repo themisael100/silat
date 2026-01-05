26d6edd22451a68b4ea8a4fde6936a77e6e13d70
themisael100 <misalbryan@gmail.com>
2026-01-05 16:38:44 -0600

    feat(wwwroot/js): Creación de una función para mostrar un breadcrumb.

---
+++ wwwroot/js/breadcrumb.js
@@ -0,0 +1,12 @@
+const breadcrumbContainer = document.getElementById("breadcrumb-container");
+
+function updateBreadcrumb() {
+    breadcrumbContainer.innerHTML = "";
+    breadcrumbContainer.innerHTML = '<li class="breadcrumb-item"><a href="/">Home</a></li>';
+    if (controllerName !== "Home") {
+        breadcrumbContainer, innerHTML +=
+            '<li class="breadcrumb-item"><a href = "/${controllerName}">${controllerName}</a></li>';
+        breadcrumbContainer.innerHTML += '<li class="breadcrumb-item active" airia-current="page">${actionName}</li>';
+    }
+}
+updateBreadcrumb();

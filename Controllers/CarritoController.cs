fa000c9c42a0d0635ecf81e87267e53634d12f97
themisael100 <misalbryan@gmail.com>
2026-01-17 17:30:27 -0600

    feat(Controllers): Creación de los metodos para procesar la compra con Paypal.

--- Controllers/CarritoController.cs
+++ Controllers/CarritoController.cs
@@ -1,6 +1,14 @@
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Tasks.Deployment.Bootstrapper;
+using Microsoft.CodeAnalysis.Elfie.Serialization;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using PayPalCheckoutSdk.Core;
+using PayPalCheckoutSdk.Orders;
+using PayPalHttp;
 using silat.Data;
 using silat.Models;
 using silat.Models.ViewModels;
@@ -25,18 +33,15 @@ namespace silat.Controllers
                 {
                     item.Producto = producto;
                     if (!producto.Activo)
-                        item.Cantidad = 0;
+                        carritoViewModel.Items.Remove(item);
                     else
                         item.Cantidad = Math.Min(item.Cantidad, producto.Stock);
-
-                    if (item.Cantidad == 0)
-                        item.Cantidad = 1;
-
                 }
                 else
                     item.Cantidad = 0;
             }
-            
+            carritoViewModel.Total = carritoViewModel.Items.Sum(item => item.Subtotal);
+
             var usuarioId = 0;
             if (User.Identity?.IsAuthenticated == true)
             {
@@ -100,6 +105,165 @@ namespace silat.Controllers
         private async Task RemoveCarritoViewModelAsync()
         {
             await Task.Run(() => Response.Cookies.Delete("carrito"));
+        }
+
+        private readonly string clientId = "";
+        private readonly string clientSecret = "";
+
+        public IActionResult ProcederConCompra(decimal montoTotal, int direccionIdSeleccionada)
+        {
+            if (direccionIdSeleccionada > 0)
+            {
+                Response.Cookies.Append(
+                    "direccionSeleccionada", direccionIdSeleccionada.ToString(),
+                    new CookieOptions { Expires = DateTimeOffset.Now.AddDays(1) }
+                );
+            }
+            else
+                return View("Index");
+
+            var request = new OrdersCreateRequest();
+            request.Prefer("return=representation");
+            request.RequestBody(BuildResquestBody(montoTotal));
+
+            var enviroment = new SandboxEnvironment(clientId, clientSecret);
+            var clien = new PayPalHttpClient(enviroment);
+
+            try
+            {
+                var response = clien.Execute(request).Result;
+                var statusCode = response.StatusCode;
+                var responseBody = response.Result<Order>();
+
+                var approveLink = responseBody.Links.FirstOrDefault(x => x.Rel == "approve");
+                if (approveLink != null)
+                    return Redirect(approveLink.Href);
+                else
+                    return RedirectToAction("Error");
+            }
+            catch (HttpException e)
+            {
+
+                return (IActionResult)e;
+            }
+        }
+
+        private OrderRequest BuildResquestBody(decimal montoTotal)
+        {
+            var baseUrl = $"{Request.Scheme}://{Request.Host}";
+            var request = new OrderRequest()
+            {
+                CheckoutPaymentIntent = "CAPTURE",
+                PurchaseUnits = new List<PurchaseUnitRequest>()
+                {
+                    new PurchaseUnitRequest()
+                    {
+                        AmountWithBreakdown = new AmountWithBreakdown()
+                        {
+                            CurrencyCode = "USD",
+                            Value=montoTotal.ToString("F2")
+                        }
+                    }
+                },
+                ApplicationContext = new ApplicationContext()
+                {
+                    ReturnUrl = $"{baseUrl}/Carrito/PagoCompletado",
+                    CancelUrl = $"{baseUrl}/Carrito/Index"
+                }
+            };
+            return request;
+        }
+
+        public IActionResult PagoCompletado()
+        {
+            try
+            {
+                var carritoJson = Request.Cookies["carrito"];
+                int direccionId = 0;
+
+                if (Request.Cookies.TryGetValue("direccionseleccionada", out string? cookieValue) && int.TryParse(cookieValue, out int parseValue))
+                {
+                    direccionId = parseValue;
+                }
+
+                List<ProductoIdAndCantidad>? productoIdAndCantidads = !string.IsNullOrEmpty(carritoJson) ? JsonConvert.DeserializeObject<List<ProductoIdAndCantidad>>(carritoJson) : null;
+
+                CarritoViewModel carritoViewModel = new();
+
+                if (productoIdAndCantidads != null)
+                {
+                    foreach (var item in productoIdAndCantidads)
+                    {
+                        var producto = _context.Productos.Find(item.ProductoId);
+                        if (producto != null)
+                        {
+                            carritoViewModel.Items.Add(
+                                new CarritoItemViewModel
+                                {
+                                    ProductoId = producto.ProductoId,
+                                    Nombre = producto.Nombre,
+                                    Precio = producto.Precio,
+                                    Cantidad = item.Cantidad
+                                }
+                            );
+                        }
+                    }
+                }
+
+                var usuarioId = 0;
+                if (User.Identity?.IsAuthenticated == true)
+                {
+                    var claim = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+                    if (int.TryParse(claim, out var id))
+                        usuarioId = id;
+                }
+
+                carritoViewModel.Total = carritoViewModel.Items.Sum(i => i.Subtotal);
+                var pedido = new Pedido
+                {
+                    UsuarioId = usuarioId,
+                    Fecha = DateTime.UtcNow,
+                    Estado = "Vendido",
+                    DireccionIdSeleccionada = direccionId,
+                    Total = carritoViewModel.Total
+                };
+
+                _context.Pedidos.Add(pedido);
+                _context.SaveChanges();
+
+                foreach (var item in carritoViewModel.Items)
+                {
+                    var pedidoDetalle = new Detalle_Pedido
+                    {
+                        PedidoId = pedido.PedidoId,
+                        ProductoId = item.ProductoId,
+                        Cantidad = item.Cantidad,
+                        Precio = item.Precio
+                    };
+                    _context.DetallePedidos.Add(pedidoDetalle);
+
+                    var producto = _context.Productos.FirstOrDefault(p => p.ProductoId == item.ProductoId);
+                    if (producto != null)
+                        producto.Stock -= item.Cantidad;
+                }
+                _context.SaveChanges();
+
+                Response.Cookies.Delete("carrito");
+                Response.Cookies.Delete("direccionseleccionada");
+
+                ViewBag.DetallePedidos = _context.DetallePedidos
+                .Where(dp => dp.PedidoId == pedido.PedidoId)
+                .Include(dp => dp.Producto)
+                .ToList();
+
+                return View("PagoCompletado", pedido);
+            }
+            catch (Exception e)
+            {
+
+                return HandleError(e);
+            }
         }
     }
 }
